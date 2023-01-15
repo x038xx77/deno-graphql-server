117a8d8d8ff2219278bfb42fe10259ab27a73432
x038xx77 <x038xx77@gmail.com>
2023-01-15 23:17:09 +0800

    inttwtttwi

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import { PrismaClient, config } from "../libPackage.ts";
 
 
 const { DATABASE_URL } = config({ safe: true }); //only Debug
-const env = await config({ safe: true })
+// const env = await config({ safe: true })
 console.log("ENV", env["DATABASE_URL"]);
 
 export const prisma = new PrismaClient({
