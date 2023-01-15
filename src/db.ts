a8436614cd6f56f051ef1d850ba27d2b5ec74c32
x038xx77 <x038xx77@gmail.com>
2023-01-15 22:46:05 +0800

    intrt

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@
 import { PrismaClient, config } from "../libPackage.ts";
 
 
-const { DATABASE_URL } = config();
+const { DATABASE_URL } = config({ safe: true });
 
 
 export const prisma = new PrismaClient({
