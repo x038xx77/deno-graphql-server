7feba070ec3b130b0c8e76f9730a87aee80c3b98
x038xx77 <x038xx77@gmail.com>
2023-01-15 22:56:27 +0800

    intrtte

--- src/db.ts
+++ src/db.ts
@@ -3,12 +3,13 @@ import { PrismaClient, config } from "../libPackage.ts";
 
 
 const { DATABASE_URL } = config({ safe: true }); //only Debug
-
+const env = await config({ safe: true })
+// console.log("ENV", env);
 
 export const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: Deno.env.get("DATABASE_URL"), //from prod
+      url: env.DATABASE_URL, //from prod
     },
   },
 });
