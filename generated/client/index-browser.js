e1c1c17aec7857e425de81436ff9922ad7e290a1
x038xx77 <x038xx77@gmail.com>
2023-01-16 01:34:17 +0800

    dev

--- generated/client/index-browser.js
+++ generated/client/index-browser.js
@@ -89,14 +89,186 @@ Prisma.NullTypes = {
 // https://github.com/microsoft/TypeScript/issues/3192#issuecomment-261720275
 function makeEnum(x) { return x; }
 
+exports.Prisma.AccountScalarFieldEnum = makeEnum({
+  id: 'id',
+  compoundId: 'compoundId',
+  userId: 'userId',
+  providerType: 'providerType',
+  providerId: 'providerId',
+  providerAccountId: 'providerAccountId',
+  refreshToken: 'refreshToken',
+  accessToken: 'accessToken',
+  accessTokenExpires: 'accessTokenExpires',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt'
+});
+
+exports.Prisma.AddressScalarFieldEnum = makeEnum({
+  id: 'id',
+  zip: 'zip',
+  deliveryId: 'deliveryId'
+});
+
+exports.Prisma.AuthorProductScalarFieldEnum = makeEnum({
+  id: 'id',
+  email: 'email',
+  username: 'username',
+  first_name: 'first_name',
+  last_name: 'last_name',
+  is_subscribed: 'is_subscribed',
+  phone_user: 'phone_user',
+  trademark: 'trademark',
+  locale_user: 'locale_user',
+  image_url_user: 'image_url_user',
+  image_file_user: 'image_file_user',
+  auth_is_telegram: 'auth_is_telegram',
+  auth_is_yandex: 'auth_is_yandex',
+  auth_is_google: 'auth_is_google',
+  is_admin_constructor: 'is_admin_constructor'
+});
+
+exports.Prisma.CategoryScalarFieldEnum = makeEnum({
+  id: 'id',
+  name: 'name',
+  productId: 'productId'
+});
+
+exports.Prisma.CityScalarFieldEnum = makeEnum({
+  id: 'id',
+  name: 'name',
+  addressId: 'addressId'
+});
+
+exports.Prisma.DeliveryScalarFieldEnum = makeEnum({
+  id: 'id',
+  title: 'title'
+});
+
+exports.Prisma.MessageScalarFieldEnum = makeEnum({
+  id: 'id',
+  body: 'body',
+  createdAt: 'createdAt',
+  userId: 'userId'
+});
+
+exports.Prisma.OrderScalarFieldEnum = makeEnum({
+  id: 'id',
+  order_id: 'order_id',
+  purchaseDate: 'purchaseDate',
+  description: 'description',
+  userId: 'userId',
+  productId: 'productId',
+  deliveryId: 'deliveryId'
+});
+
+exports.Prisma.Payments_productbuyersScalarFieldEnum = makeEnum({
+  id: 'id',
+  amount: 'amount',
+  backUrl: 'backUrl',
+  companyName: 'companyName',
+  details: 'details',
+  emailReq: 'emailReq',
+  errorCode: 'errorCode',
+  merchantEmail: 'merchantEmail',
+  merchantName: 'merchantName',
+  message: 'message',
+  orderId: 'orderId',
+  paymentId: 'paymentId',
+  phoneReq: 'phoneReq',
+  success: 'success',
+  tranDate: 'tranDate'
+});
+
+exports.Prisma.ProductScalarFieldEnum = makeEnum({
+  id: 'id',
+  title: 'title',
+  description: 'description',
+  image: 'image',
+  sku: 'sku',
+  quantity: 'quantity',
+  price: 'price',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+  userId: 'userId'
+});
+
+exports.Prisma.Product_categoryScalarFieldEnum = makeEnum({
+  id: 'id',
+  name: 'name'
+});
+
+exports.Prisma.ProductsProductResultScalarFieldEnum = makeEnum({
+  id: 'id',
+  userId: 'userId',
+  name: 'name',
+  image: 'image',
+  text: 'text',
+  price_product: 'price_product',
+  taxation: 'taxation',
+  tax: 'tax',
+  authorProductId: 'authorProductId',
+  productsProductId: 'productsProductId'
+});
+
+exports.Prisma.ProductsProductScalarFieldEnum = makeEnum({
+  id: 'id',
+  count: 'count',
+  next: 'next',
+  previous: 'previous'
+});
+
+exports.Prisma.ProductsProductWhoLikesItScalarFieldEnum = makeEnum({
+  id: 'id',
+  product_id: 'product_id',
+  user_id: 'user_id'
+});
+
+exports.Prisma.Products_product_categoryScalarFieldEnum = makeEnum({
+  id: 'id',
+  product_id: 'product_id',
+  category_id: 'category_id'
+});
+
+exports.Prisma.Products_product_tagsScalarFieldEnum = makeEnum({
+  id: 'id',
+  producy_id: 'producy_id',
+  tag_id: 'tag_id'
+});
+
+exports.Prisma.Products_tagScalarFieldEnum = makeEnum({
+  id: 'id',
+  name: 'name',
+  color: 'color',
+  slug: 'slug',
+  key: 'key'
+});
+
 exports.Prisma.QueryMode = makeEnum({
   default: 'default',
   insensitive: 'insensitive'
 });
 
+exports.Prisma.SessionScalarFieldEnum = makeEnum({
+  id: 'id',
+  userId: 'userId',
+  expires: 'expires',
+  sessionToken: 'sessionToken',
+  accessToken: 'accessToken',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt'
+});
+
 exports.Prisma.SortOrder = makeEnum({
   asc: 'asc',
   desc: 'desc'
+});
+
+exports.Prisma.TagsScalarFieldEnum = makeEnum({
+  id: 'id',
+  name: 'name',
+  color: 'color',
+  slug: 'slug',
+  productsProductResultId: 'productsProductResultId'
 });
 
 exports.Prisma.TransactionIsolationLevel = makeStrictEnum({
@@ -106,15 +278,77 @@ exports.Prisma.TransactionIsolationLevel = makeStrictEnum({
   Serializable: 'Serializable'
 });
 
+exports.Prisma.UserFavoritesProductScalarFieldEnum = makeEnum({
+  id: 'id',
+  is_favorite: 'is_favorite',
+  userId: 'userId',
+  productId: 'productId'
+});
+
 exports.Prisma.UserScalarFieldEnum = makeEnum({
   id: 'id',
   email: 'email',
-  name: 'name'
+  password: 'password',
+  username: 'username',
+  firstName: 'firstName',
+  lastName: 'lastName',
+  phoneUser: 'phoneUser',
+  trademark: 'trademark',
+  localeUser: 'localeUser',
+  imageUrlUser: 'imageUrlUser',
+  authIsTelegram: 'authIsTelegram',
+  authIsYandex: 'authIsYandex',
+  authIsGoogle: 'authIsGoogle',
+  role: 'role',
+  bio: 'bio',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+  emailDateVerified: 'emailDateVerified'
+});
+
+exports.Prisma.VerificationRequestScalarFieldEnum = makeEnum({
+  id: 'id',
+  identifier: 'identifier',
+  token: 'token',
+  expires: 'expires',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+  userId: 'userId'
+});
+
+exports.Prisma.VoteScalarFieldEnum = makeEnum({
+  id: 'id',
+  count_votes: 'count_votes',
+  is_vote_user: 'is_vote_user',
+  productId: 'productId',
+  userId: 'userId'
 });
 
 
 exports.Prisma.ModelName = makeEnum({
-  User: 'User'
+  Message: 'Message',
+  Account: 'Account',
+  Session: 'Session',
+  City: 'City',
+  Address: 'Address',
+  Category: 'Category',
+  Product: 'Product',
+  Delivery: 'Delivery',
+  VerificationRequest: 'VerificationRequest',
+  product_category: 'product_category',
+  User: 'User',
+  ProductsProduct: 'ProductsProduct',
+  ProductsProductResult: 'ProductsProductResult',
+  Order: 'Order',
+  products_product_category: 'products_product_category',
+  products_tag: 'products_tag',
+  products_product_tags: 'products_product_tags',
+  ProductsProductWhoLikesIt: 'ProductsProductWhoLikesIt',
+  payments_productbuyers: 'payments_productbuyers',
+  AuthorProduct: 'AuthorProduct',
+  Tags: 'Tags',
+  UserFavoritesProduct: 'UserFavoritesProduct',
+  Vote: 'Vote'
 });
 
 /**
--- src/schema.ts
+++ src/schema.ts
@@ -4,66 +4,168 @@ export const schema = createSchema({
     typeDefs: /* GraphQL */ `
       type Query {
         users: [User!]
+        products: ProductsResponseApi
       }
       type Mutation {
        
         logIn(input: UserLoginInput) : AuthPayload
-        
+              
       }
+
       input UserLoginInput {
-        email: String
-        name: String
+        email: String!
+        password: String!
        
       }
       input UserSignUpInput {
         email: String
-        name: String!
+        password: String
+        username: String!
       }
+
       input UserUpdateInput {
         email: String
-        name: String!
+        password: String!
+        username: String!
       }
+
       input UserSelect {
         id: String!
         email: String!
-        name: String!
+        password: String!
       }
       
       type AuthPayload {
         user: User
         token: String
       }
+
       type UserUpdatePayload {
         user: User
       }
        
       type User {
-        id: String! 
-        email: String! 
-        name: String
+        accounts: [Account!]!
+        email: String
+        first_name: String!
+        id: String
+        last_name: String!
+        messages: [Message!]
+        password: String!
+        phone_user: String!
+        products: [ProductApi!]
+        username: String!
+      }
+
+      type Message {
+        body: String!
+        id: ID!
+        messagedBy: User!
+        messagedById: ID!
+      }
+
+      type Account {
+        accessToken: String!
+        accessTokenExpires: String!
+        accountedBy: User!
+        refreshToken: String!
+      }
+      
+      type ProductsResponseApi {
+        count: ID
+        next: ID
+        previous: ID
+        results: [ProductApi]
+      }
+
+      type ProductApi {
+        amount_cart_product: Boolean
+        author: AuthorProductApi!
+        id: ID
+        id_cart_product: Boolean
+        image: String!
+        is_favorited: Boolean
+        is_in_shopping_cart: Boolean
+        name: String!
+        price_product: ID!
+        tags: [TagsProductApi!]
+        tax: String!
+        taxation: String!
+        text: String!
+      }
+
+      type AuthorProductApi {
+        auth_is_google: Boolean
+        auth_is_telegram: Boolean
+        auth_is_yandex: Boolean
+        email: String
+        first_name: String!
+        id: ID!
+        image_file_user: String!
+        image_url_user: String!
+        is_admin_constructor: Boolean
+        is_subscribed: Boolean
+        last_name: String!
+        locale_user: String!
+        phone_user: String!
+        trademark: String!
+        username: String!
+      }
+
+      type TagsProductApi {
+        color: String!
+        id: ID!
+        name: String!
+        slug: String!
+      }
+      
+      type Vote {
+        count_votes: ID!
+        id: ID
+        is_vote_user: Boolean
+        productId: String
+        userId: String
+      }
+
+      type VoteInput {
+        productId: String
+        userId: String
+      }
+
+      type VotePayload {
+        count_votes: ID!
+        is_vote_user: Boolean
+        productId: String
+        userId: String
+      }
+      
+      type UserFavoritesProduct {
+        is_favorite: Boolean
+        productId: String
+        userId: String
       }
   `,
     resolvers: {
        
       Query: {
-        // async products(parent: unknown, args: {}, context: GraphQLContext) {
+        async products(parent: unknown, args: {}, context: GraphQLContext) {
   
-        //   const result = await fetch("https://buildberries.pythonanywhere.com/api/products/?limit=999", {
-        //     method: 'GET',
-        //     headers: {
-        //       "Content-Type": "application/json"
-        //     },
-        //   }).then((res: any) => res.json())
+          const result = await fetch("https://buildberries.pythonanywhere.com/api/products/?limit=999", {
+            method: 'GET',
+            headers: {
+              "Content-Type": "application/json"
+            },
+          }).then((res: any) => res.json())
   
-        //   return result
-        // },
+          return result
+        },
   
         //start Users
         users: (parent: unknown, args: {}, context: GraphQLContext) => {
           // if (context.currentUser === null) { throw new Error('Unauthenticated!') }
                     
           const users = context.prisma.user.findMany()
-          return users
+          return {users}
         },
        // end users
       },
@@ -73,8 +175,8 @@ export const schema = createSchema({
 
   
         // end SignUp mutation
+
         // Login
-  
         async logIn(
           parent: unknown,
           args: { input: { email: string; name: string; } },
