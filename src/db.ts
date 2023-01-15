d1fdda7cb4586e5445bb5d73f3ba38c7f06ca4d5
x038xx77 <x038xx77@gmail.com>
2023-01-15 22:41:11 +0800

    intr

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,14 @@
 
 import { PrismaClient, config } from "../libPackage.ts";
 
-const envVars = await config();
+
+const { DATABASE_URL } = config();
+
 
 export const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: "prisma://aws-us-east-1.prisma-data.com/?api_key=eXRONbDtIGZiX5vbzP3i9iJDu4mgINZL1YrDChZDrUw4Egn4eqOsqw1j4FQGEx1t",
+      url: DATABASE_URL,
     },
   },
 });
