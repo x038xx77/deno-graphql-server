57d87b2bcdd4d47cf9c5d23e93f02514d5de05cc
x038xx77 <x038xx77@gmail.com>
2023-01-16 01:42:12 +0800

    devw

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,8 @@
 
 import { PrismaClient } from "../libPackage.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+// import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-const { DATABASE_URL } = config({ safe: true }); //only Debug
+// const { DATABASE_URL } = config({ safe: true }); //only Debug
 // const env = await config({ safe: true })
 // console.log("ENV", env["DATABASE_URL"]);
 
