ed443158b274f36dbbfe0b2965a5896b5d60c5a6
x038xx77 <x038xx77@gmail.com>
2023-01-16 21:15:58 +0800

    tdev2

--- libPackage.ts
+++ libPackage.ts
@@ -1,34 +1,32 @@
-    //@ts-ignore
+ 
     export { serve } from "https://deno.land/std@0.172.0/http/mod.ts";
     // import { type Request as typeRequest } from "https://deno.land/std@0.172.0/http/mod.ts";
     // export {typeRequest }
-    import { type Response as typeResponse } from "https://deno.land/x/serve@1.0.5/deps.ts";
-    export { typeResponse }
+    // import { type Response as typeResponse } from "https://deno.land/x/serve@1.0.5/deps.ts";
+    // export { typeResponse }
 
 
-
-     //@ts-ignore
     export { Prisma, PrismaClient } from "./generated/client/deno/edge.js";
-     //@ts-ignore
+  
     export { createYoga, createSchema } from 'https://esm.sh/graphql-yoga@3.1.1?deps=graphql@16.6.0';
-     //@ts-ignore
+
      export { Context } from "https://deno.land/x/oak/mod.ts";
 
-   //@ts-ignore
+ 
      import SchemaBuilder from 'https://esm.sh/@pothos/deno/packages/core/mod.ts';
         // for pothos plugins, you should pin both 'graphql' and '@pothos/core'
-    //@ts-ignore
+  
     // export  { createSchema } from 'https://cdn.skypack.dev/graphql-yoga?dts'
     import RelayPothosPlugin from 'https://esm.sh/@pothos/deno/packages/plugin-relay/mod.ts';
-     //@ts-ignore
+
     export { SchemaBuilder, RelayPothosPlugin }
-     //@ts-ignore
+  
      import { getNumericDate,
       create as jwtCreate,
       decode as jwtDecode,
       verify as jwtVerify,} from "https://deno.land/x/djwt@v2.0/mod.ts";
     export { getNumericDate, jwtCreate, jwtDecode, jwtVerify }
-//@ts-ignore
+
 import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
 export {bcrypt}
 
