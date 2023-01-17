d540f186abeaa47722daf8d369796526575a48ff
x038xx77 <x038xx77@gmail.com>
2023-01-17 12:03:44 +0800

    newdev18

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,29 @@ import { authContext } from "./context.ts";
 
 const yoga = createYoga({
   schema: schema,
-  context:authContext
+  context:authContext,
+  cors: {
+    origin: 'https://xn--brilliant-p985h.ml',
+    credentials: true,
+    allowedHeaders: ['X-Custom-Header'],
+    methods: ['GET','POST', 'OPTIONS']
+  }
 });
 
 serve(yoga,{ port: 8000 });
+
+// const server_port = 5500;
+
+// function req_handler(req: Request): Response {
+//   console.log("\nReceived a request...\n");
+//   const body = JSON.stringify({ message: "I am a DENO server 🦕" });
+//   return new Response(body, {
+//     status: 200,
+//     headers: {
+//       "content-type": "application/json; charset=utf-8",
+//     },
+//   });
+// }
+
+// serve(req_handler, { port: server_port})
+// console.log("Listening on PORT: ", server_port);
