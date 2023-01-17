1f3ca1ddb6b57721597b07ec381b33404d3b3aa6
x038xx77 <x038xx77@gmail.com>
2023-01-17 15:32:59 +0800

    newd corrrhh888

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const yoga = createYoga({
   cors: {
     origin: 'https://xn--brilliant-p985h.ml',
     credentials: true,
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: ['Content-Type', 'application/json', 'authorization'],
     methods: ['GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'],  }
 
 });
